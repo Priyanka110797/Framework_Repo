4ee9aba9643acaf6439c0e903c62b7e52766c7d6
Admin <Admin@DESKTOP-JUGL5UQ>
2026-04-27 04:53:36 -0700

    narayan 1st commit..

---
+++ MavenDemoProject/src/test/java/PracticeMaven1/EclipsePush.java
@@ -0,0 +1,12 @@
+package PracticeMaven1;
+
+import org.testng.annotations.Test;
+
+public class EclipsePush {
+
+	@Test
+	public void orgTest() {
+		
+		System.out.println("welcome to selenium");
+	}
+}
